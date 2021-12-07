68c5035ba7cc3be90613d06120ae6c7c3729109b
Mostafa Amini Nasab <mostafa@backbase.com>
2021-12-07 22:45:21 +0100

    Implement BAAM

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fbd99432cdeeb7cbb444";
+/******/ 	var hotCurrentHash = "30856e3983a57fb9d7a2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
