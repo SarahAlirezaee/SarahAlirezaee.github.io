daac4cc25c6e79253a6e4ca0ac8c3730a422d35a
Mostafa Amini Nasab <mostafa@backbase.com>
2021-12-08 12:30:09 +0100

    Add playstore link

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "380ca026743f004dd525";
+/******/ 	var hotCurrentHash = "2fa630699cbf26bb0312";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
