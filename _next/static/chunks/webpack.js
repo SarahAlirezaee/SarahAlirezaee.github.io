db20b0c3d93ecea534bd9202111f5070f365b3fd
Mostafa Amini Nasab <mostafa@backbase.com>
2021-12-08 12:20:45 +0100

    Make all pages repsponsive

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "30856e3983a57fb9d7a2";
+/******/ 	var hotCurrentHash = "380ca026743f004dd525";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
